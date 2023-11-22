7e2f90a7a7ba8b8561c8a1208ccb75a29e846688
Kuba Jasko <kuba.jasko@version1.com>
2023-11-22 11:54:00 +0000

    INITIAL

---
+++ lib/advent/of/code/2022/24.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "24/version"
+
+module Advent
+  module Of
+    module Code
+      module 2022
+        module 24
+          class Error < StandardError; end
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ spec/advent/of/code/2022/24_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Advent::Of::Code::2022::24 do
+  it "has a version number" do
+    expect(Advent::Of::Code::2022::24::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
