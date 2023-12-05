93864f3a95812c6a7ac020174f299b04c6ca9611
Kuba Jasko <kuba.jasko@version1.com>
2023-12-05 11:34:10 +0000

    2022 9 - COMPLETE - 1 & 2

--- lib/bauk/advent_of_code/year_2022/challenge_09/challenge.rb
+++ lib/bauk/advent_of_code/year_2022/challenge_09/challenge.rb
@@ -11,16 +11,12 @@ module Bauk
           def initialize
             super
             @lines = File.readlines File.join(__dir__, Opts.file), chomp: true
-            # @map = Map.new 2, 2
-            # puts @map
-            # puts @map.cell(0, 0).inspect
-            # @knots = (0..9).map { {row: 0, column: 0}}
-            @head_row = 0
-            @head_column = 0
-            @tail_row = 0
-            @tail_column = 0
+          end
+
+          def reset(knots)
+            @knots = (0..knots).map { {row: 0, column: 0 }}
             @tail_visits = {}
-            @tail_visits["#{@tail_row}_#{@tail_column}"] = {row: @tail_row, column: @tail_column} # Starting position
+            update_tail_visits
             @max_row = 0
             @max_column = 0
             @min_row = 0
@@ -28,15 +24,17 @@ module Bauk
           end
 
           def show_map
-            @max_row = [@max_row, @head_row, @tail_row].max
-            @max_column = [@max_column, @head_column, @tail_column].max
-            @min_row = [@min_row, @head_row, @tail_row].min
-            @min_column = [@min_column, @head_column, @tail_column].min
+            @max_row = [@max_row, @knots[0][:row]].max
+            @max_column = [@max_column, @knots[0][:column]].max
+            @min_row = [@min_row, @knots[0][:row]].min
+            @min_column = [@min_column, @knots[0][:column]].min
             map = Map.new @max_row + 1 - @min_row, @max_column + 1 - @min_row
             map.allow_multiples = true
-            map.insert @head_row - @min_row, @head_column - @min_column, "H"
-            map.insert @tail_row - @min_row, @tail_column - @min_column, "T"
+            @knots.each_with_index do |knot, index|
+              map.insert knot[:row] - @min_row, knot[:column] - @min_column, index
+            end
             puts map.to_s_with_border
+            sleep 1
           end
 
           def run
@@ -45,50 +43,72 @@ module Bauk
             star_two
           end
 
-          def update_tail
-            row_diff = @head_row - @tail_row
-            column_diff = @head_column - @tail_column
-            logger.debug { "Update start) H: #{@head_row}/#{@head_column} T:#{@tail_row}/#{@tail_column}" }
+          def update_tail_visits
+            @tail_visits["#{@knots[-1][:row]}_#{@knots[-1][:column]}"] = {row: @knots[-1][:row], column: @knots[-1][:column]}
+          end
+
+          def update_knots
+            (1...@knots.length).each do |i|
+              update_knot @knots[i], @knots[i-1]
+            end
+            update_tail_visits
+          end
+
+          def update_knot(knot, head) # rubocop:disable Metrics/AbcSize
+            row_diff = head[:row] - knot[:row]
+            column_diff = head[:column] - knot[:column]
+            logger.debug { "Update start) H: #{head[:row]}/#{head[:column]} T:#{knot[:row]}/#{knot[:column]}" }
             return if row_diff.between?(-1, 1) && column_diff.between?(-1, 1)
 
             if column_diff.negative?
-              @tail_column -= 1
+              knot[:column] -= 1
             elsif column_diff.positive?
-              @tail_column += 1
+              knot[:column] += 1
             end
             if row_diff.negative?
-              @tail_row -= 1
+              knot[:row] -= 1
             elsif row_diff.positive?
-              @tail_row += 1
+              knot[:row] += 1
             end
-            @tail_visits["#{@tail_row}_#{@tail_column}"] = {row: @tail_row, column: @tail_column}
-            logger.debug { "Update end  ) H: #{@head_row}/#{@head_column} T:#{@tail_row}/#{@tail_column}" }
+            logger.debug { "Update end  ) H: #{head[:row]}/#{head[:column]} T:#{knot[:row]}/#{knot[:column]}" }
+          end
+
+          def update_head(move)
+            case move
+            when "U"
+              @knots[0][:row] -= 1
+            when "D"
+              @knots[0][:row] += 1
+            when "L"
+              @knots[0][:column] -= 1
+            when "R"
+              @knots[0][:column] += 1
+            end
           end
 
           def star_one
+            reset 1
+            run_star
+            logger.warn "Star one answer: #{@tail_visits.length}"
+          end
+
+          def run_star
             @lines.each do |line|
               die "Invalid line: #{line}" unless line =~ /^([UDLR]) ([0-9]+)$/
 
               (1..$2.to_i).each do
-                case $1
-                when "U"
-                  @head_row -= 1
-                when "D"
-                  @head_row += 1
-                when "L"
-                  @head_column -= 1
-                when "R"
-                  @head_column += 1
-                end
-                update_tail
+                update_head $1
+                update_knots
                 # show_map
               end
             end
-            logger.warn "Star one answer: #{@tail_visits.length}"
+            logger.debug "Tail poisitions: #{@tail_visits.keys.inspect}"
           end
 
           def star_two
-            logger.warn "Star two answer: "
+            reset 9
+            run_star
+            logger.warn "Star two answer: #{@tail_visits.length}"
           end
         end
       end
