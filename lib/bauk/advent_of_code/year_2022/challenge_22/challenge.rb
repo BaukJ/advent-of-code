2bfa5dc1ae8e129463b7e2cd112be669e02f2b5d
Kuba Jasko <kuba.jasko@version1.com>
2023-12-05 17:13:55 +0000

    2022 22 - more progress?...

--- lib/bauk/advent_of_code/year_2022/challenge_22/challenge.rb
+++ lib/bauk/advent_of_code/year_2022/challenge_22/challenge.rb
@@ -36,21 +36,8 @@ module Bauk
 
             @map.insert @row, @column, @facing_symbols[@facing_index]
             puts @map.to_s_with_border
-            @map.reset_cell @row, @column
-            sleep 0.5
-          end
-
-          def move(steps)
-            (0...steps).each do
-              case @facings[@facing_index]
-              when "U" then move_up
-              when "D" then move_down
-              when "L" then move_left
-              when "R" then move_right
-              else die "Invalid direction: #{direction}"
-              end
-              show_map
-            end
+            @map.remove @row, @column, @facing_symbols[@facing_index]
+            sleep 0.1
           end
 
           def turn(direction)
@@ -64,58 +51,134 @@ module Bauk
             show_map
           end
 
+          def move(steps)
+            (1..steps).each do |i|
+              logger.debug { "Moving #{@facings[@facing_index]} from #{@row}/#{@column} (step #{i}/#{steps})"}
+              set_succesful_cell if @map.empty? @row, @column
+              case @facings[@facing_index]
+              when "U" then move_up
+              when "D" then move_down
+              when "L" then move_left
+              when "R" then move_right
+              else die "Invalid direction: #{direction}"
+              end
+              show_map
+            end
+          end
+
+          def move_back
+            case @facings[@facing_index]
+            when "U" then move_down
+            when "D" then move_up
+            when "L" then move_right
+            when "R" then move_left
+            else die "Invalid direction: #{direction}"
+            end
+          end
+
+          def set_succesful_cell
+            @last_row = @row
+            @last_column = @column
+          end
+
+          def revert_to_succesful_move
+            @row = @last_row
+            @column = @last_column
+          end
+
+          def calculate_new_square
+            logger.debug { "Calculating new sqaure from #{@row}/#{@column}"}
+            if @box_shape
+              move_around_square
+            else
+              @column = 0 if @column > @map.column_max_index
+              @column = @map.column_max_index if @column.negative?
+              @row = @map.row_max_index if @row.negative?
+              @row = 0 if @row > @map.row_max_index
+              # Find next free space
+              move 1 if @map.cell(@row, @column).include? " "
+            end
+          end
+
+          def move_around_square
+            @mappings = {
+              "3_0" => {
+                source: {direction: "U", row: 3, column: 0},
+                destination: {direction: "D", row: 0, column: 11},
+              }
+            }
+            if Opts.file == "data.txt"
+            else
+              if @mappings["#{@row}_#{@column}"]
+                mapping = @mappings["#{@row}_#{@column}"]
+                die "Invalid direction (#{@facings[@facing_index]})" unless @facings[@facing_index] == mapping[:source][:direction]
+                @facing_index = @facings.find_index mapping[:destination][:direction]
+                @row = mapping[:destination][:row]
+                @column = mapping[:destination][:column]
+              elsif @row == -1
+                @row = 4
+                @column = 3 - (@column - 8)
+                @facing_index = 1
+              elsif @column == 12 && @row.between?(0, 3)
+                @column = 15
+                @row = 11 - @row
+                @facing_index = 1
+              elsif @column == 12 && @row.between?(4, 7)
+                @column = 12 + 7 - @row
+                @row = 8
+                @facing_index = 2
+              elsif @column == 16 && @row.between?(8, 11)
+                @column = 11
+                @row = 11 - @row
+                @facing_index = 2
+              elsif @row == 12 && @column.between?(12, 15)
+                @row = 4 + 15 - @column
+                @column = 0
+                @facing_index = 2
+              else
+                die "Invalid position #{@row}/#{@column}"
+              end
+              check_move
+            end
+          end
+
           def move_right
             @column += 1
-            @column = 0 if @column > @map.column_max_index
+            check_move
+          end
+
+          def check_move
             # If new position is empty, success, if it's a wall, move back left to where you last were, otherwise we're in a void and we keep going
-            if @map.empty?(@row, @column) then nil
-            elsif @map.cell(@row, @column).include?("#") then move_left
-            else
-              move_right
+            if @row.negative? || @row > @map.row_max_index || @column.negative? || @column > @map.column_max_index || @map.cell(@row, @column).include?(" ")
+              calculate_new_square
+            elsif @map.cell(@row, @column).include?("#") then revert_to_succesful_move
+            elsif @map.empty?(@row, @column) then set_succesful_cell
+            else die "Invalid state"
             end
           end
 
           def move_left
             @column -= 1
-            @column = @map.column_max_index if @column.negative?
-            # If new position is empty, success, if it's a wall, move back left to where you last were, otherwise we're in a void and we keep going
-            if @map.empty?(@row, @column) then nil
-            elsif @map.cell(@row, @column).include?("#") then move_right
-            else
-              move_left
-            end
+            check_move
           end
 
           def move_up
             @row -= 1
-            @row = @map.row_max_index if @row.negative?
-            # If new position is empty, success, if it's a wall, move back left to where you last were, otherwise we're in a void and we keep going
-            if @map.empty?(@row, @column) then nil
-            elsif @map.cell(@row, @column).include?("#") then move_down
-            else
-              move_up
-            end
+            check_move
           end
 
           def move_down
             @row += 1
-            @row = 0 if @row > @map.row_max_index
-            # If new position is empty, success, if it's a wall, move back left to where you last were, otherwise we're in a void and we keep going
-            if @map.empty?(@row, @column) then nil
-            elsif @map.cell(@row, @column).include?("#") then move_up
-            else
-              move_down
-            end
+            check_move
           end
 
           def run
             logger.warn("Starting challenge #{self.class.name}")
-            star_one
+            # star_one
             star_two
           end
 
-          def star_one
-            move_right # To make sure we start on the first valid square
+          def do_route
             route = @route.clone
             until route.empty?
               if route.sub!(/^([A-Z])/, "")
@@ -126,11 +189,23 @@ module Bauk
                 die "Invalid route: #{route}"
               end
             end
+          end
+
+          def star_one
+            move_right # To make sure we start on the first valid square
+            set_succesful_cell
+            do_route
             logger.warn "Star one answer: #{((@row + 1) * 1000) + ((@column + 1) * 4) + @facing_index}"
           end
 
           def star_two
-            logger.warn "Star two answer: "
+            @row = 0
+            @column = 0
+            move_right # To make sure we start on the first valid square
+            set_succesful_cell
+            @box_shape = true
+            do_route
+            logger.warn "Star two answer: #{((@row + 1) * 1000) + ((@column + 1) * 4) + @facing_index}"
           end
         end
       end
