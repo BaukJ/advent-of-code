13ca16c7cea135f12b735c1258195773ba99fe54
Kuba Jasko <kuba.jasko@version1.com>
2023-12-21 11:00:37 +0000

    rubocop

--- lib/bauk/advent_of_code/year_2023/challenge_21/challenge.rb
+++ lib/bauk/advent_of_code/year_2023/challenge_21/challenge.rb
@@ -12,7 +12,7 @@ module Bauk
             super
             @lines = File.readlines File.join(__dir__, Opts.file), chomp: true
             @map = Map.from_lines @lines
-            start = @map.cells_with_row_column.select { |cell,r,c| cell.include? "S" }.last
+            start = @map.cells_with_row_column.select { |cell, _r, _c| cell.include? "S" }.last
             @row = start[1]
             @column = start[2]
             @map.remove @row, @column, "S"
@@ -20,13 +20,13 @@ module Bauk
           end
 
           def steps(count)
-            positions = [{row: @row, column: @column}]
+            positions = [{ row: @row, column: @column }]
             count.times do
               # show_positions(positions)
               new_positions = []
               positions.each do |position|
                 @map.adjacent_4_cells_with_row_column(position[:row], position[:column]).each do |cell, row, column|
-                  new_positions << {row:, column:} if cell.empty?
+                  new_positions << { row:, column: } if cell.empty?
                 end
               end
               positions = new_positions.uniq
@@ -36,7 +36,7 @@ module Bauk
           end
 
           def steps2(count) # rubocop:disable Metrics/AbcSize
-            positions = [{row: @row, column: @column}]
+            positions = [{ row: @row, column: @column }]
             @map = @original_map.deep_clone
             @map.insert @row, @column, :E
             even = true
@@ -47,7 +47,7 @@ module Bauk
               positions.each do |position|
                 @map.adjacent_4_cells_with_row_column(position[:row], position[:column]).each do |cell, row, column|
                   if cell.empty?
-                    new_positions << {row:, column:}
+                    new_positions << { row:, column: }
                     @map.insert row, column, even ? :E : :e
                   end
                 end
@@ -55,12 +55,12 @@ module Bauk
               positions = new_positions.uniq
               check_map_size(positions)
             end
-            @map.cells.select { |cell| cell.include? (even ? :E : :e) }.length
+            @map.cells.select { |cell| cell.include?(even ? :E : :e) }.length
           end
 
           def steps3(count) # rubocop:disable Metrics/AbcSize
-            gone = {"#{@row}_#{@column}" => :E}
-            positions = [{row: @row, column: @column}]
+            gone = { "#{@row}_#{@column}" => :E }
+            positions = [{ row: @row, column: @column }]
             @map = @original_map.deep_clone
             even = true
             old_position_count = 0
@@ -70,14 +70,14 @@ module Bauk
             gone_true_counts = []
             gone_false_counts = []
             count.times do |round|
-              logger.info { "#{round}) Gone: #{gone.length}, Positions: #{positions.length}" } if round % 10 == 0
-              if (round-65) % 131 == 0
+              logger.info { "#{round}) Gone: #{gone.length}, Positions: #{positions.length}" } if (round % 10).zero?
+              if ((round - 65) % 131).zero?
                 gone_count = gone.length
                 gone_true_count = gone.values.select { |v| v == :E }.length
-                gone_false_count = gone.values.select { |v| v != :E }.length
+                gone_false_count = gone.values.reject { |v| v == :E }.length
                 gone_true_counts << gone_true_count
                 gone_false_counts << gone_false_count
-                gone_all_counts << {E: gone_true_count, e: gone_false_count, even: }
+                gone_all_counts << { E: gone_true_count, e: gone_false_count, even: }
                 gone_counts << gone_count
                 if gone_counts.length > 9
                   find_pattern(gone_true_counts)
@@ -119,19 +119,19 @@ module Bauk
 
           def find_pattern(list)
             pattern = [list]
-            until pattern[-1].all? { |n| n.zero? } || pattern[-1].length < 2
+            until pattern[-1].all?(&:zero?) || pattern[-1].length < 2
               puts pattern[-1].inspect
               from = pattern[-1]
               pattern << []
-              (0...(from.length-1)).each do |i|
-                pattern[-1] << (from[i+1] - from[i])
+              (0...(from.length - 1)).each do |i|
+                pattern[-1] << (from[i + 1] - from[i])
               end
             end
           end
 
           # DEPENDS on there being a straight line to the ends, and there being exactly the same steps to each end!
           # AND! the number you want being 131*n + 65....
-          # Pattern is... gone = 
+          # Pattern is... gone =
           def steps4(count) # rubocop:disable Metrics/AbcSize
             count -= 65
             rounds = count / 131
@@ -153,10 +153,10 @@ module Bauk
             # find_pattern pattern.map { |p| p[:E] }.each_slice(2).map(&:first)
             # find_pattern pattern.map { |p| p[:E] }.each_slice(2).map(&:last)
             # exit
-            
-            incrementer = 118968
+
+            incrementer = 118_968
             even = false
-            pattern = [[{e: 3776, E: 3642}, {e: 33248, E: 33652}], [{e: 89494, E: 88954}, {e: 148438, E: 148978}]]
+            pattern = [[{ e: 3776, E: 3642 }, { e: 33_248, E: 33_652 }], [{ e: 89_494, E: 88_954 }, { e: 148_438, E: 148_978 }]]
             # puts "ROUNDS: #{rounds}"
             rounds.times do
               # puts pattern.inspect
@@ -176,10 +176,10 @@ module Bauk
 
           def get_4_moves(row, column)
             [
-              {row:, column: column + 1},
-              {row:, column: column - 1},
-              {row: row + 1, column:},
-              {row: row - 1, column:}
+              { row:, column: column + 1 },
+              { row:, column: column - 1 },
+              { row: row + 1, column: },
+              { row: row - 1, column: }
             ]
           end
 
@@ -196,14 +196,14 @@ module Bauk
                 add_map_down
               elsif position[:column] == @map.column_max_index
                 add_map_right
-              elsif position[:row] == 0
+              elsif (position[:row]).zero?
                 positions.each do |pos|
                   pos[:row] += @map.row_count
                 end
                 add_map_up
                 check_map_size(positions)
                 break
-              elsif position[:column] == 0
+              elsif (position[:column]).zero?
                 positions.each do |pos|
                   pos[:column] += @map.column_count
                 end
@@ -217,28 +217,40 @@ module Bauk
 
           def add_map_right
             @map.deep_clone.columns.each do |column|
-              column.each { |c| c.delete(:e); c.delete(:E) }
+              column.each do |c|
+                c.delete(:e)
+                c.delete(:E)
+              end
               @map.insert_column(-1, column)
             end
           end
 
           def add_map_left
             @map.deep_clone.columns.reverse.each do |column|
-              column.each { |c| c.delete(:e); c.delete(:E) }
+              column.each do |c|
+                c.delete(:e)
+                c.delete(:E)
+              end
               @map.insert_column(0, column)
             end
           end
 
           def add_map_down
             @map.deep_clone.rows.each do |row|
-              row.each { |c| c.delete(:e); c.delete(:E) }
+              row.each do |c|
+                c.delete(:e)
+                c.delete(:E)
+              end
               @map.insert_row(-1, row)
             end
           end
 
           def add_map_up
             @map.deep_clone.rows.reverse.each do |row|
-              row.each { |c| c.delete(:e); c.delete(:E) }
+              row.each do |c|
+                c.delete(:e)
+                c.delete(:E)
+              end
               @map.insert_row(0, row)
             end
           end
@@ -268,7 +280,6 @@ module Bauk
           end
 
           def star_two
-            test = 65
             # logger.warn "Star one answer: #{steps3(1000000)}"
             # logger.warn "Star one answer: #{steps4(1000)}"
             # while test < 1000
@@ -277,7 +288,7 @@ module Bauk
             #   logger.warn "Star step4 answer: #{steps4(test)}"
             #   test += 131
             # end
-            logger.warn "Star one answer: #{steps4(26501365)}"
+            logger.warn "Star one answer: #{steps4(26_501_365)}"
           end
         end
       end
