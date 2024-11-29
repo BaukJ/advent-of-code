2fa8e9566359c6da9e25c231e95d3cdc0ada3064
Kuba Jasko <kuba.jasko@version1.com>
2024-11-29 13:54:44 +0000

    Make template.sh safer

--- scripts/template.sh
+++ scripts/template.sh
@@ -3,13 +3,21 @@ export YEAR=$1
 export CHALLENGE=$2
 if [[ ! "$CHALLENGE" ]]
 then
+    echo "USAGE: $0 YEAR CHALLENGE"
     exit 3
 fi
 template_dir="scripts/template"
 challenge_dir="lib/bauk/advent_of_code/year_$YEAR/challenge_$CHALLENGE"
 echo "Doing $YEAR/$CHALLENGE ($challenge_dir)"
-echo "Enter to continue..."
-read
+if [[ -d "$challenge_dir" ]]
+then
+    echo "WARNING: Already exists: $challenge_dir"
+    read -p "Enter 'y' to continue: " ans
+    if [[ "$ans" != "y" ]]
+    then
+        exit 3
+    fi
+fi
 
 mkdir -p "$challenge_dir"
 
@@ -17,3 +25,4 @@ for f in $(ls "$template_dir")
 do
     cat "$template_dir/$f" | envsubst >"$challenge_dir/${f/.envsubst}"
 done
+echo "DONE"
