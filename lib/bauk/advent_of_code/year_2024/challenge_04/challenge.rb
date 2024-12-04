6a52924ab8730ef3f36ee32528ae3bfebe8dca26
Kuba Jasko <jakub@windsor.ai>
2024-12-04 22:20:48 +0000

    2024 04 - COMPLETE 1

---
+++ lib/bauk/advent_of_code/year_2024/challenge_04/challenge.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative "../../base_challenge"
+
+module Bauk
+  module AdventOfCode
+    module Year2024
+      module Challenge04
+        # Challenge for 2024/04
+        class Challenge < BaseChallenge
+          def initialize
+            super
+            @lines = File.readlines File.join(__dir__, Opts.file), chomp: true
+            @map = @lines.map(&:chars)
+          end
+
+          def run
+            logger.warn("Starting challenge #{self.class.name}")
+            star_one if [0, 1].include? Opts.star
+            star_two if [0, 2].include? Opts.star
+          end
+
+          def star_one
+            total = 0
+            @map.each_with_index do |cells, row|
+              cells.each_with_index do |cell, column|
+                next unless cell == "X"
+
+                lines = [
+                  [{ row:, column: column + 1 }, { row:, column: column + 2 }, { row:, column: column + 3 }],
+                  [{ row:, column: column - 1 }, { row:, column: column - 2 }, { row:, column: column - 3 }],
+                  [{ row: row + 1, column: }, { row: row + 2, column: }, { row: row + 3, column: }],
+                  [{ row: row - 1, column: }, { row: row - 2, column: }, { row: row - 3, column: }],
+                  [{ row: row - 1, column: column - 1 }, { row: row - 2, column: column - 2 }, { row: row - 3, column: column - 3 }],
+                  [{ row: row + 1, column: column - 1 }, { row: row + 2, column: column - 2 }, { row: row + 3, column: column - 3 }],
+                  [{ row: row - 1, column: column + 1 }, { row: row - 2, column: column + 2 }, { row: row - 3, column: column + 3 }],
+                  [{ row: row + 1, column: column + 1 }, { row: row + 2, column: column + 2 }, { row: row + 3, column: column + 3 }]
+                ]
+                lines.each do |positions|
+                  next if positions[2][:row].negative? || positions[2][:column].negative?
+                  next unless @map[positions[0][:row]]&.[](positions[0][:column]) == "M" &&
+                              @map[positions[1][:row]]&.[](positions[1][:column]) == "A" &&
+                              @map[positions[2][:row]]&.[](positions[2][:column]) == "S"
+
+                  total += 1
+                end
+                # puts "Found X at: #{row}/#{column}"
+              end
+            end
+            logger.warn "Star one answer: #{total}"
+          end
+
+          def star_two
+            logger.warn "Star two answer: "
+          end
+        end
+      end
+    end
+  end
+end
